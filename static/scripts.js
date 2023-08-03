a5a8b516556a1983da5111c2232aa6aa5d103606
Samuel <mbuthia.jsamuel@gmail.com>
2023-08-03 08:40:14 +0300

    Landing page styles

--- static/scripts.js
+++ static/scripts.js
@@ -6,3 +6,21 @@ function showHideDiv(id) {
         e.style.display = "none";
     }
 }
+
+//modal dialog
+function displayDialog(){
+    var button = document.getElementById('open');
+    var close = document.getElementById('close');
+    var modal = document.getElementById('modal');
+    button.addEventListener('click', function (event) {
+        event.preventDefault();
+        modal.style.display = 'block';
+    });
+}
+
+function closeDialog(){
+    close.addEventListener('click', function (event) {
+        event.preventDefault();
+        modal.style.display = 'none';
+    });
+}
