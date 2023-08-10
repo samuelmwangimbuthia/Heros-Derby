500dc0325b66ec488004b8fbe8aac78c38e7dab1
Samuel <mbuthia.jsamuel@gmail.com>
2023-08-10 09:40:53 +0300

    style the signup form and the landing page image

--- static/scripts.js
+++ static/scripts.js
@@ -8,15 +8,14 @@ function showHideDiv(id) {
 }
 
 //modal dialog
-function displayDialog(){
-    var button = document.getElementById('open');
-    var close = document.getElementById('close');
-    var modal = document.getElementById('modal');
-    button.addEventListener('click', function (event) {
-        event.preventDefault();
-        modal.style.display = 'block';
-    });
-}
+var button = document.getElementById('open');
+var close = document.getElementById('close');
+var modal = document.getElementById('modal');
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    modal.style.display = 'block';
+});
+
 
 function closeDialog(){
     close.addEventListener('click', function (event) {
